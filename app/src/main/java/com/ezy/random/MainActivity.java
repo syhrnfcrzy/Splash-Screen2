de5a38f7a5e0199209413a001e8870763d62889c
syhrn <syahrunfacrezy@gmail.com>
2020-07-20 08:33:22 +0900

    Hello-3

---
+++ app/src/main/java/com/ezy/random/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ezy.random;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    TextView TextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        TextView =findViewById(R.id.textView);
+
+        TextView.setAnimation(bottomAnim);
+    }
+}
